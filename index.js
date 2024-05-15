0fc9e477c0eb031c80603ef0e631792c7ac213a6
Mauricio IC <mauricio_ic19@outlook.com>
2024-05-15 08:33:50 -0600

    added body parser support

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
+const db = require("./config/database.js");
 const express = require("express");
 const app = express();
 
-app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
 
 app.get("/", (req, res, next) => {
   res.render("index");
 });
 
-app.get("/pokemon", (req, res, next) => {
-  res.json({ nombre: "Mauricio", edad: "26" });
+app.get("/pokemon", async (req, res, next) => {
+  const pokemon = await db.query("SELECT * FROM pokemon;");
+  console.log(pokemon);
+  res.json(pokemon);
+});
+
+app.post("/pokemon", (req, res, next) => {
+  console.log(req.body);
+  res.json(req.body);
 });
 
 app.listen(3000, () => {
